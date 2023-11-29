5afc549750790a566d21fbfbc80fb8e6299b45ff
Derek Wisong <derekwisong@gmail.com>
2023-11-29 18:28:45 -0500

    Refactorings

--- datascape/dataset.py
+++ datascape/dataset.py
@@ -1,5 +1,7 @@
 """Dataset tools.
 """
+import datetime
+import time
 
 from typing import Any, Iterable, List, Tuple
 
@@ -17,7 +19,8 @@ class Dataset:
     def __init__(self,
                  location,
                  *,
-                 partition_on: List[Tuple[str, Any] | str] = None
+                 partition_on: List[Tuple[str, Any] | str] = None,
+                 partition_flavor: str = "hive"
                  ) -> None:
         """Initialize a Dataset object.
 
@@ -26,14 +29,19 @@ class Dataset:
             partition_on: (optional). List of field names to partition on.
         """
         self.location = location
+        self.partitioning = ds.partitioning(
+            self._fields_to_schema(partition_on),
+            flavor=partition_flavor) if partition_on else None
+        self.dataset = ds.dataset(location, partitioning=self.partitioning)
 
-        if partition_on:
-            part_schema = pa.schema([(p, pa.string()) if isinstance(p, str) else p for p in partition_on])
-            self.dataset = ds.dataset(
-                location,
-                partitining=ds.partitioning(part_schema, flavor="hive"))
-        else:
-            self.dataset = ds.dataset(location)
+    def _fields_to_schema(self, partition_on: List[Tuple[str, Any] | str]) -> pa.Schema:
+        return pa.schema([(p, pa.string()) if isinstance(p, str) else p for p in partition_on])
+
+    def _get_filename_template(self):
+        nanos = time.time_ns()
+        timestamp = datetime.datetime.fromtimestamp(nanos // 1_000_000_000)
+        remaining_nanos = nanos % 1_000_000_000
+        return f"part-{timestamp.strftime('%Y%m%dT%H%M%S')}.{remaining_nanos}-{{i}}.parquet"
 
     def add_records(self, records: Iterable[Any]) -> None:
         raise NotImplementedError("Not yet implemented")
